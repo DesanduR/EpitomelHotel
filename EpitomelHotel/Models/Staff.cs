6e98fb51a156f3e89f3510c3ba8b52b00684fde9
ac121625 <ac121625@8CG8163KZN>
2025-04-09 10:09:52 +1200

    test cases

--- EpitomelHotel/Models/Staff.cs
+++ EpitomelHotel/Models/Staff.cs
@@ -15,7 +15,7 @@ namespace EpitomelHotel.Models
         public string Firstname { get; set; }
         [Required, MinLength(1), MaxLength(20), RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Lastname required.")]
         public string Lastname { get; set; }
-        [Required, MinLength(1), MaxLength(20), RegularExpression(@"^[a-zA-Z0-9 ]+$", ErrorMessage = "Profession required.")]
+        [Required, MinLength(1), MaxLength(20), RegularExpression(@"^[a-zA-Z ]+$", ErrorMessage = "Profession required.")]
         public string Profession { get; set; }
         [Required, MinLength(1), MaxLength(20), RegularExpression(@"^\+?\d{1,3}[- ]?\(?\d{3}\)?[- ]?\d{3}[- ]?\d{4}$", ErrorMessage = "Phonenumber required.")]
         public string Phonenumber { get; set; }
